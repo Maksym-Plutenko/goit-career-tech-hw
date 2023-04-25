bf47a7b13a7486d0d269dc1ac430c8ddb33a9ce6
Maksym-Plutenko <plutenkom@gmail.com>
2023-04-26 01:11:54 +0300

    fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,33 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import {Card} from './Card/Card';
 
+axios.defaults.baseURL='https://644845bde7eb3378ca2b7ea5.mockapi.io/users/';
+
 export const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await axios.get('/users');
+        console.log(response);
+        console.log(response.data);
+        setUsers(response.data);
+        // return response.data;
+      } catch (error) {
+        // return e.message;
+        // console.log(e.message);
+        throw new Error(error.message);
+      }
+    }
+
+    fetchContacts();
+    // console.log('opanki');
+    // console.log(users);
+  }, []);
+
   return (
     <div
       // style={{
